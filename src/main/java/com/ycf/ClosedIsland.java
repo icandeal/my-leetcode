f9dcdbd42764434c8538ffb19687c577a7afad36
Sniper <539513963@qq.com>
2020-07-08 21:58:44 +0800

    修改项目名称

--- src/main/java/com/ycf/ClosedIsland.java
+++ src/main/java/com/ycf/ClosedIsland.java
@@ -1,8 +1,17 @@
 package com.ycf;
 
+import org.junit.Test;
+
+/**
+ * Created by yuchunfan on 2020/7/3.
+ *
+ * LeetCode 1254. 统计封闭岛屿的数目
+ * 难度：中等
+ */
 public class ClosedIsland {
 
-    public static void main(String[] args) {
+    @Test
+    public static void test() {
         int[][] grid = new int[][]{
                 {1,1,1,1,1,1,1},
                 {1,0,0,0,0,0,1},
