d731ef1558b60f43db9f78a85425b02663fab77f
Sniper <539513963@qq.com>
2024-03-11 22:57:22 +0800

    提交作业

---
+++ src/main/java/com/ycf/leetcode/q383/Solution.java
@@ -0,0 +1,35 @@
+package com.ycf.leetcode.q383;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+/**
+ * Created by yuchunfan on 2024/3/10
+ */
+
+public class Solution {
+  public static void main(String[] args) {
+    String ransomNote = "aa", magazine = "ab";
+    Solution solution = new Solution();
+    System.out.println(solution.canConstruct(ransomNote, magazine));
+  }
+
+  public boolean canConstruct(String ransomNote, String magazine) {
+    char[] chars = magazine.toCharArray();
+    int[] charMap = new int[26];
+    for (int i = 0; i < chars.length; i++) {
+      char aChar = chars[i];
+      charMap[aChar - 'a'] ++;
+    }
+
+    char[] array = ransomNote.toCharArray();
+    for (int i = 0; i < array.length; i++) {
+      char c = array[i];
+      int i1 = c - 'a';
+      charMap[i1] --;
+      if (charMap[i1] < 0) return false;
+    }
+    return true;
+  }
+}
